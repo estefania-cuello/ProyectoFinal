cc92c8b922575ce2ff5c078fca663bbdf598f069
estefania <ecuello44@gmail.com>
2023-02-27 18:23:15 -0300

    Agregado de Botón y favicon con cambios en estilos

---
+++ funcion.js
@@ -0,0 +1,9 @@
+  
+document.getElementById('boton').onclick = function()
+{
+    
+   document.getElementById("demo").innerHTML="Celular: (431) 277-34971           Email: emmaarg@example.com         Referencias disponibles bajo petición";
+}
+
+
+   
